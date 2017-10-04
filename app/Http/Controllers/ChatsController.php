230956e1a7defd51188be8747e5a802c430cd7b8
cemilcelik <cemilcelik84@gmail.com>
2017-10-04 18:15:48 +0300

    Broadcast to chat channel when the message save to the database.

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Events\MessageSent;
 
 class ChatsController extends Controller
 {
@@ -26,9 +27,11 @@ class ChatsController extends Controller
     {
         $user = Auth::user();
 
-        $user->messages()->create([
+        $message = $user->messages()->create([
             'message' => $request->input('message')
         ]);
+
+        broadcast(new MessageSent($user, $message))->toOthers();
 
         return ['status' => 'Message Sent!'];
     }
