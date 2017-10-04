86fc9da8c6d8f59c50f94b7d648306c01c56d0af
cemilcelik <cemilcelik84@gmail.com>
2017-10-04 16:39:43 +0300

    Create ChatsController.php

---
+++ app/Http/Controllers/ChatsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChatsController extends Controller
+{
+    public function index()
+    {
+        # code...
+    }
+
+    public function fetchMessages()
+    {
+        # code...
+    }
+
+    public function sendMessages(Type $var = null)
+    {
+        # code...
+    }
+}
